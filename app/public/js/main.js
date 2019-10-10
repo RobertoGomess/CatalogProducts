0e3bacb0de24d927eddbf9e793f6a05124d5e8bf
Roberto gomes dos santos junior <r.b.gomes.dos.santos@avanade.com.br>
2019-10-10 15:39:28 -0300

    Load e animação de cards

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,9 +1,22 @@
 M.AutoInit();
 
-document.addEventListener('DOMContentLoaded', function() {
+$(window).on('load', function() {
     
     $("#loading").fadeOut(1000);
 
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
+
+    $('.expandir-card').on('click', function(){
+      var card = $(this).closest('.card');
+      console.log($(this).parent().parent().parent());
+      if(card.hasClass('card-expandido')){
+        card.removeClass('card-expandido');
+        $(this).text('fullscreen');
+      }else{
+        card.addClass('card-expandido');
+        $(this).text('fullscreen_exit');
+      }
+    });
+
   });
