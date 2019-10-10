647298f94860bae55260154211732ccca40884d2
Roberto gomes dos santos junior <r.b.gomes.dos.santos@avanade.com.br>
2019-10-10 12:09:37 -0300

    Preload das paginas

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,6 +1,9 @@
 M.AutoInit();
 
 document.addEventListener('DOMContentLoaded', function() {
+    
+    $("#loading").fadeOut(1000);
+
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
